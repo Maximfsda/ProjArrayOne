c5a85799a8a447eca4e40b521b7204c9efc418a6
Maximfsda <lisitsin.maxi@yandex.ru>
2022-09-27 16:00:08 +1000

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("TaskOne");
+        int [] integers = new int {1,2,3};
+        float [] fractionalNumbers = new float {1.57, 7.654, 9.986};
+        int [] integerArray= new int [2];
+        integerArray [0] = 0;
+        integerArray [1] = 1;
+        integerArray [2] = 2;
+        System.out.println();
+        System.out.println("TaskTwo");
 
 
     }
